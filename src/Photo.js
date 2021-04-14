d5c8ccea27f5e4cfa191518d16fd2f9570b0d4af
Meerduke <Meerduke@users.noreply.github.com>
2021-04-14 16:40:41 +0200

    unrequired logos removed

--- src/Photo.js
+++ src/Photo.js
@@ -5,7 +5,7 @@ export default function Photo(props){
     
     return(
         <div>
-            <img src={image} alt="recipe image" className= "img-fluid rounded m-3 d-none d-md-inline"/>
+            <img src={image} alt="recipe example" className= "img-fluid rounded m-3 d-none d-md-inline"/>
         </div>
     )
 };
